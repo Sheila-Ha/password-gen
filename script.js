815dd794a2ba9b28e31c688897973f942f682133
sheilahanson <slhanson11@live.com>
2023-11-10 23:59:17 -0600

    mistakenly thought all my previous commits were saving - password gen is working properly

--- script.js
+++ script.js
@@ -11,9 +11,44 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var password = 'sheila';
+  var password = '';
+  var passwordLength = prompt('Enter a length for your password from 8-128 characters');
+  var numericLength = parseInt(passwordLength);
+  if (numericLength < 8 || numericLength > 128){
+    alert('Length must be between 8 and 128 characters');
+    return '';
+  }
+  var useLowerCase = confirm('use lowercase letters in password');
+  var useUpperCase = confirm('use uppercase letters in password');
+  var useNumeric = confirm('use a number in password');
+  var useSpecialCharacter = confirm('use a special character in password');
+  if (!useLowerCase && !useUpperCase && !useNumeric && !useSpecialCharacter) {
+    alert('you must use at least one character type');
+    return '';
+  }
+  var lowerCase = 'abcdefghigkmnopqrstuvwxyz'; //omitted lowercase L because it can look the same depending on font used
+  var upperCase = 'ABCDEFGHJKLMNOPQRSTUVWXYZ'; //omitted uppercase I because it can look the same depending on the font used
+  var numbers = '23456789'; //omitted 0 & 1 because it can look the same depending on the font used
+  var symbols = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+  var characterCandidates = '';
+  if (useLowerCase) {
+    characterCandidates += lowerCase;
+  }
+  if (useUpperCase) {
+    characterCandidates += upperCase;
+  }
+  if (useNumeric) {
+    characterCandidates += numbers;
+  }
+  if (useSpecialCharacter) {
+    characterCandidates += symbols;
+  }
+  for(var i=0; i<passwordLength; i++) {
+    var characterIndex = Math.floor(Math.random() * characterCandidates.length-1);
+    var randomSelectedCharacter = characterCandidates.charAt(characterIndex);
+    password += randomSelectedCharacter;
+  }
   return password;
-
 }
 
 
